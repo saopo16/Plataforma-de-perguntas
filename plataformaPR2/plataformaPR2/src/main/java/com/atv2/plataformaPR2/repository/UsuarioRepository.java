eadd8e28f97919c863767bd62af18e2a9d21f4e9
Sara P <saportugal2006@gmail.com>
2024-05-17 19:13:09 -0300

    U-repo

---
+++ plataformaPR2/plataformaPR2/src/main/java/com/atv2/plataformaPR2/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atv2.plataformaPR2.repository;
+
+/**
+ *
+ * @author prden
+ */
+public class UsuarioRepository {
+    
+}
